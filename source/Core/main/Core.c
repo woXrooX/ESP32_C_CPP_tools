b0eb3628c62c8f6b8cb0c1fbe67bfd821369085d
woXrooX <woXrooX@gmail.com>
2025-12-01 06:34:35 +0500

    Clean up

--- source/Core/main/Core.c
+++ source/Core/main/Core.c
@@ -1,16 +1,14 @@
 #include "woXrooX/LED_LOGGER.h"
 #include "woXrooX/WiFi.h"
-#include "woXrooX/Button_PTT.h"
-#include "woXrooX/MIC.h"
-#include "woXrooX/WebSocket_client.h"
+#include "woXrooX/Button.h"
+// #include "woXrooX/MIC.h"
+// #include "woXrooX/WebSocket_client.h"
 // #include "woXrooX/HTTP_client.h"
 
 void app_main(void) {
 	if (LEDs_init() != 0) return;
 
 	// if (init_WiFi() != 0) return;
-
-	button_init();
 
 	// MIC_listen_start();
 
